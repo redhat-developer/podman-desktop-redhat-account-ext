9322552cd96de00ba3fe9025456b863cf75ec8fe
Denis Golovin <dgolovin@redhat.com>
2024-02-02 11:58:13 -0800

    fix: add dependencies to rhsm and cra client packages
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- src/registry/models/SummariesV1.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { Stream_ServiceAccountV1_ } from './Stream_ServiceAccountV1_';
-export type SummariesV1 = {
-    services?: Stream_ServiceAccountV1_;
-};
-
--- src/rhsm/models/exportJobResponse.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type exportJobResponse = {
-    exportID?: string;
-    href?: string;
-};
-
