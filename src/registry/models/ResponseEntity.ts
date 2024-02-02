1ccf46f6a21fb8cfaac81441accff92ba31cc940
Denis Golovin <dgolovin@redhat.com>
2024-02-02 11:58:13 -0800

    fix: add dependencies to rhsm and cra client packages
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- src/registry/models/ResponseEntity.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type ResponseEntity = {
-    body?: any;
-    status?: any;
-};
-
--- src/rhsm/models/Date.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-/**
- * Date format used in API responses.
- */
-export type Date = string;
