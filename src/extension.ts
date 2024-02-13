a7cbaf4488feba0f79f7c06f43125d430d9d6059
Denis Golovin <dgolovin@redhat.com>
2024-02-12 21:34:59 -0800

    fix: replase function with arrow function
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -51,7 +51,7 @@ async function getAuthService() {
 function parseJwt (token: string) {
   var base64Url = token.split('.')[1];
   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(Buffer.from(base64, 'base64').toString().split('').map(function(c) {
+  var jsonPayload = decodeURIComponent(Buffer.from(base64, 'base64').toString().split('').map((c) => {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   }).join(''));
 
